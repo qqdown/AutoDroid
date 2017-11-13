37958d90382d41922d2f087606514eb1e713d404
wmFrank <1090616897@qq.com>
2017-11-13 16:49:01 +0800

    test

--- src/edu/nju/autodroid/strategy/DepthGroupWeightedStrategy.java
+++ src/edu/nju/autodroid/strategy/DepthGroupWeightedStrategy.java
@@ -26,6 +26,7 @@ import java.util.function.Predicate;
 public class DepthGroupWeightedStrategy implements IStrategy {
     private GroupTransaction groupTransaction = new GroupTransaction();
 
+    //test
     protected String runtimePackage;
     protected IAndroidAgent androidAgent;
     private Action lastAction = null;
